2e9971bcf27dfd7a2d82999c5d8f1bf7a23fd5eb
Keith Resar <keith.resar@centurylinkcloud.com>
2016-10-21 21:16:13 -0500

    -

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 echo "Welcome to the best application in the world.\n";
 echo "<br>All running on openshift\n";
 
-echo "<hr>\n"
+echo "<hr>\n";
 
 echo "Serving from <b>".$_SERVER['HOSTNAME']."\n";
 
