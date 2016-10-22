f4e183ef51fcd7da24bcabc8ff069d614066fe25
Keith Resar <keith.resar@centurylinkcloud.com>
2016-10-21 21:20:43 -0500

    -

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ echo "<br>All running on openshift\n";
 
 echo "<hr>\n";
 
-echo "Serving from <b>".$_SERVER['HOSTNAME']."\n";
+echo "Serving from pod <b>".getenv('HOSTNAME')."</b>.\n";
 
+echo "<hr>\n";
 
 ?>
+
+<a href="dbtest.php">dbtest</a> · <a href="genload.php">genload</a>
+
