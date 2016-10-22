51996e24f863d1888674d494df65af6fa21c7ba9
Keith Resar <keith.resar@centurylinkcloud.com>
2016-10-21 21:06:47 -0500

    -

---
+++ genload.php
@@ -0,0 +1,13 @@
+<?php
+
+$EXEC_TIME_SECS=5;
+
+echo "Generating load....<br>\n";
+
+$start_ts = time();
+while (time()-$start_ts<$EXEC_TIME_SECS)  true;
+
+echo "Generating load....complete after ".(time()-$start_ts)." seconds.<br>\n";
+
+
+?>
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
-echo "Welcome to the best application in the world.";
-echo "<br>All running on openshift";
+echo "Welcome to the best application in the world.\n";
+echo "<br>All running on openshift\n";
+
+echo "<hr>\n"
+
+echo "Serving from <b>".$_SERVER['HOSTNAME']."\n";
 
 
 ?>
